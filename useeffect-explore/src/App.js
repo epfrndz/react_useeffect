ee22c1d36ede74513c426109f03e63cfefebd0bb
Edward <epfrndz@gmail.com>
2022-09-21 23:07:44 -0700

    testing react useEffect

--- useeffect-explore/src/App.js
+++ useeffect-explore/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+  const [testType, setTestType] = useState('1')
+  
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resourceType])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <div>
+        <button onClick={() => setTestType('1')}>1</button>
+        <button onClick={() => setTestType('2')}>2</button>
+        <button onClick={() => setTestType('3')}>3</button>
+      </div>
+      <h1>{resourceType}</h1>
+      <h1>{testType}</h1>
+      {items.map(item =>{
+        return <pre key={item.id} >{JSON.stringify(item)}</pre>
+      })}
+    </>
+    );
 }
 
 export default App;
